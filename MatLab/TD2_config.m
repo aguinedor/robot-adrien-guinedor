1766f8dc717ba4fc797516ac2fa2f6d73a28e3c9
IUT GEII Toulon Table 6 <43076999+FloRide1@users.noreply.github.com>
2021-09-29 16:28:46 +0200

    matlab_TP2

--- MatLab/TD2_config.m
+++ MatLab/TD2_config.m
@@ -3,6 +3,10 @@ b = 8.5E-3;
 K = 0.5093;
 R = 0.13;
 L = 1.6E-3;
-Ki = 0;
-Kd = 0;
-Kp = 0;
+
+Kp = 20;   %20
+Kd = 0.15; %0.15
+Ki = 50;   %50
+
+%KP en premier puis KD et ensuite KI avec les deux autres à 0
+
